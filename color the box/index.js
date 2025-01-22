705103e4263bf792257daa1b081bd34aeefa403c
prottay <prottay.saha46@gmail.com>
2025-01-22 13:47:15 +0600

    code add

--- color the box/index.js
+++ color the box/index.js
@@ -1,12 +1,19 @@
+// use any selector
+// let boxes = document.querySelector('.container').children;
+// or use
+// let boxes = document.querySelectorAll('.box')
+// or use
+let boxes = document.getElementsByClassName('box')
+
+
 function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-    let mainColor = "#" + red.toString(16) + green.toString(16) + blue.toString(16);
-    return mainColor;
+    let red = Math.ceil(0 + Math.random() * 255);
+    let green = Math.ceil(0 + Math.random() * 255);
+    let blue = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`
 }
 
-document.querySelectorAll('.box').forEach(e => {
-    e.style.backgroundColor = getRandomColor()
-    e.style.color = getRandomColor()
+Array.from(boxes).forEach(e => {
+    e.style.backgroundColor = getRandomColor();
+    e.style.Color = getRandomColor();
 })
