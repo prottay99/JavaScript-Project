03086ca25ee2cea0f5080b376f3118dc167cde50
prottay <prottay.saha46@gmail.com>
2025-06-27 20:09:54 +0600

    todo app - javaScript files added

---
+++ ToDo App/app.js
@@ -0,0 +1,42 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+const addTask = document.querySelector("#add-task");
+
+addTask.addEventListener("click", () => {
+  if (inputBox.value === "") {
+    alert("Please enter a task to add a new ToDo");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  saveData();
+  inputBox.value = "";
+});
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
