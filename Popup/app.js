78741cd1ebbc791100cad483479b85109b6c1e33
prottay <prottay.saha46@gmail.com>
2025-07-10 19:29:40 +0600

    popup

---
+++ Popup/app.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector(".popup");
+
+function showPopup() {
+  popup.classList.add("show-popup");
+}
+function closePopup() {
+  popup.classList.remove("show-popup");
+}
